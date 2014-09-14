7be57959106cfd8d7e83231522b5d7eb4e9e5e37
frdo <fer.filan@gmail.com>
2014-09-14 23:18:18 +0200

    shell scripts for weka

---
+++ build_dataset/weka_classification/comma_sep_conv.py
@@ -0,0 +1,9 @@
+import csv
+
+def to_comma(input_file, output_file):
+	dict_reader = csv.DictReader(open(input_file, 'r'), delimiter=';')
+	dict_writer = csv.DictWriter(open(output_file, 'w'), delimiter=',', fieldnames=dict_reader.fieldnames)  # DELIMITER
+	dict_writer.writerow(dict((fn,fn) for fn in dict_reader.fieldnames)) #Scrive gli header
+
+	for row in dict_reader:
+		dict_writer.writerow(row)
