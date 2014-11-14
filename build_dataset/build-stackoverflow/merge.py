a5907f2d8a48d68c0e83661c08a4d921aa0c8f0d
frdo <fer.filan@gmail.com>
2014-11-14 17:00:58 +0100

    comments

--- build_dataset/build-stackoverflow/merge.py
+++ build_dataset/build-stackoverflow/merge.py
@@ -1,5 +1,24 @@
 import csv
 
+# Fonde, in un unico file csv, le colonne di due file csv. I due file vengono fusi sulla
+# base di un confronto sul campo 'PostId'.
+#
+# Esempio: i due file che vogliono fondere hanno, uno i campi PostId e Body e l'altro i
+# campi PostId, SentimentNegativeScore e SentimentPositiveScore. Il file risultante 
+# conterrà PostId, Body, SentimentNegativeScore e SentimentPositiveScore. Inoltre ogni
+# istanza, del primo e del secondo file, saranno allineate sulla base del confronto 
+# su PostId. In particolare, due istanze, provenienti dai due file, verranno allineate
+# se i due PostId sono uguali.
+#
+# parametri:
+#	file_name_input: nome del file che si vuole fondere, deve contenere almeno il campo:
+#				- 'PostId'
+#	file_name_metric: nome del file che si vuole fondere, deve contenere almeno il campo:
+#				- 'PostId'
+#	output_file: nome del file su cui scrivere, conterrà i campi:
+#				- 'PostId'
+#				- tutti i campi di file_name_input
+#				- tutti i campi di file_name_metric
 def merge(file_name_input, file_name_metric, output_file):
 	dict_reader_1 = csv.DictReader(open(file_name_input, 'r'), delimiter=';') # DELIMITER
 	dict_reader_2 = csv.DictReader(open(file_name_metric, 'r'), delimiter=';') # DELIMITER
@@ -24,16 +43,4 @@ def merge(file_name_input, file_name_metric, output_file):
 		dict_writer.writerow(row_1)
 
 	return 'Done'
-
-#merge('academia_questions.csv', 'ac_questions_acc.csv', 'temp/academia_merge_1.csv')
-#merge('temp/academia_merge_1.csv', 'ac_questions_answacc.csv', 'temp/academia_merge_2.csv')
-#merge('temp/academia_merge_2.csv', 'ac_questions_questacc.csv', 'temp/academia_merge_3.csv')
-#merge('temp/academia_merge_3.csv', 'ac_questions_answscore.csv', 'temp/academia_merge_4.csv')
-#merge('temp/academia_merge_4.csv', 'ac_questions_questscore.csv', 'temp/academia_merge_5.csv')
-#merge('temp/academia_merge_5.csv', 'ac_questions_badg.csv', 'temp/academia_merge_6.csv')
-#merge('temp/academia_merge_6.csv', 'ac_questions_week.csv', 'temp/academia_merge_7.csv')
-#merge('temp/academia_merge_7.csv', 'ac_questions_len.csv', 'temp/academia_merge_8.csv')
-#merge('temp/academia_merge_8.csv', 'ac_sentiment.csv', 'temp/academia_merge_9.csv')
-#merge('temp/academia_merge_9.csv', 'ac_liwc.csv', 'academia_final.csv')
-
 
