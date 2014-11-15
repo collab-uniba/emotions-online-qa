65d096e61cda83339def93e1cc4436b98e19c716
frdo <fer.filan@gmail.com>
2014-11-15 11:14:37 +0100

    comments

--- build_dataset/mallet/dictionary/stopwords.py
+++ build_dataset/mallet/dictionary/stopwords.py
@@ -4,6 +4,15 @@ from django.utils.encoding import smart_str
 def del_punctuation(text):
 	return text.translate(string.maketrans ("" , ""), string.punctuation)
 
+# Costruisce un dizionario, ordinato per frequenza descrescente,
+# delle parole contenute nei testi del file passato in input.
+# La frequenza viene calcolata come il numero di volte che la parola
+# compare nelle istanze diviso il numero di parole totali.
+# NON ripulisce i testi dai tag html e dal code snippet.
+#
+# parametri:
+#	file_name: file in formato csv con i campi 'Title' e 'Body' (da i quali prende il testo)
+#	output: file csv con la colonna delle parole e la colonna delle frequenze
 def create_dictionary(file_name, output):
 	print "Processing..."
 	dict_reader = csv.DictReader(open(file_name, 'r'))
@@ -16,9 +25,12 @@ def create_dictionary(file_name, output):
 		title = row['Title']
 		
 		try:
-			body_cleaned = del_punctuation(smart_str(body))
-			title_cleaned = del_punctuation(smart_str(title))
+			#body_cleaned = del_punctuation(smart_str(body))
+			#title_cleaned = del_punctuation(smart_str(title))
 			
+			body_cleaned = smart_str(body)
+			title_cleaned = smart_str(title)
+
 			corpus = body_cleaned + " " + title_cleaned
 
 			words = re.findall(r"[\w']+", corpus)
@@ -55,7 +67,16 @@ def create_dictionary(file_name, output):
 
 	return sorted_dict
 
-
+# Preso in input un dizionario delle parole con relativa frequenza, seleziona
+# tutte le parole che superano una certa soglia di frequenza.
+#
+# parametri:
+#	file_name: nome del file csv da cui leggere, deve contenere due colonne:
+#			- la prima con la parola
+#			- la seconda con il valore numerico che indica la frequenza
+#	output: nome del file txt sul quale scrivere la lista delle parole che 
+#		superano la soglia
+#	threshold: soglia sopra la quale vengono selezionate le parole
 def create_stopwords_file(file_name, output, threshold):
 	reader = csv.reader(open(file_name, 'r'))
 	writer = open(output, 'w')
