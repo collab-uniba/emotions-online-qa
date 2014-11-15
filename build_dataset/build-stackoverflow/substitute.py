125c7d3c1bccf83c6d28029142fed74bc2887cfd
frdo <fer.filan@gmail.com>
2014-11-15 18:07:48 +0100

    comments

--- build_dataset/build-stackoverflow/substitute.py
+++ build_dataset/build-stackoverflow/substitute.py
@@ -1,25 +1,21 @@
 import csv
 
-# Fonde, in un unico file csv, le colonne di due file csv. I due file vengono fusi sulla
-# base di un confronto sul campo 'PostId'.
-#
-# Esempio: i due file che vogliono fondere hanno, uno i campi PostId e Body e l'altro i
-# campi PostId, SentimentNegativeScore e SentimentPositiveScore. Il file risultante 
-# conterra' PostId, Body, SentimentNegativeScore e SentimentPositiveScore. Inoltre ogni
-# istanza, del primo e del secondo file, saranno allineate sulla base del confronto 
-# su PostId. In particolare, due istanze, provenienti dai due file, verranno allineate
-# se i due PostId sono uguali.
+# Sostituisce i campi di un file csv ad i campi di un altro file csv e scrive tutto su
+# un nuovo file csv. I due file vengono fusi sulla base di un confronto sul campo 'PostId'.
 #
 # parametri:
-#	file_name_input: nome del file che si vuole fondere, deve contenere almeno il campo:
+#	file_name_input: nome del file a cui si vogliono sostituire i campi, deve contenere almeno il campo:
 #				- 'PostId'
-#	file_name_metric: nome del file che si vuole fondere, deve contenere almeno il campo:
+#	file_name_metric: nome del file da cui prendere i campi da sostituire, deve contenere almeno il campo:
 #				- 'PostId'
 #	output_file: nome del file su cui scrivere, conterra' i campi:
 #				- 'PostId'
-#				- tutti i campi di file_name_input
-#				- tutti i campi di file_name_metric
-def substitute(file_name_input, file_name_metric, delim=';', output_file, subst_fields=[]):
+#				- tutti i campi di file_name_input aggiornati
+#	delim: delimitatore da utilizzare per aprire file_name_metric
+#	subst_fields: vettore che contiene i nomi dei campi che si vogliono sostituire, tali campi devono
+#		essere presenti sia in file_name_input sia in file_name_metric; se tale vettore e' vuoto
+#		vengono considerati tutti i campi di file_name_metric
+def substitute(file_name_input, file_name_metric, output_file, delim=';', subst_fields=[]):
 	dict_reader_1 = csv.DictReader(open(file_name_input, 'r'), delimiter=';') # DELIMITER
 	dict_reader_2 = csv.DictReader(open(file_name_metric, 'r'), delimiter=delim) # DELIMITER
 	
