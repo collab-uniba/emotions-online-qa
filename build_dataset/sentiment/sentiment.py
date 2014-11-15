8e612a95a45124aca570fbbb968afc2600cecd4f
frdo <fer.filan@gmail.com>
2014-11-15 08:08:44 +0100

    comments

--- build_dataset/sentiment/sentiment.py
+++ build_dataset/sentiment/sentiment.py
@@ -1,13 +1,20 @@
-import csv, re
-from HTMLParser import HTMLParser
+import csv
 import string
 import jpype
 import os
-from django.utils.encoding import smart_str
 
 body_test = "The worldwide diffusion of social media has profoundly changed the way we communicate and access information. Increasingly, people try to solve domain-specific problems through interaction on social online Question and Answer (Q&A) sites. The enormous success of Stack Overflow, a community of 2.9 million programmers asking and providing answers about code development, attests this increasing trend. One of the biggest drawbacks of communication through social media is to appropriately convey sentiment through text. While display rules for emotions exist and are widely accepted for traditional face-to-face interaction, people might not be prepared for effectively dealing with the barriers of social media to non-verbal communication. Though, emotions matter, especially in the context of online Q&A communities where social reputation is a key factor for successful knowledge sharing. As a consequence, the design of systems and mechanisms for fostering emotional awareness in computer-mediated communication is becoming an important technical and social challenge for research in computer-supported collaborative work and social computing."
 
 
+# Calcola il sentiment score positivo e negativo per il testo che si vuole
+# analizzare.
+#
+# parametri:
+#	corpus: testo che si vuole analizzare
+#
+# output:
+#	restituisce una stringa con il sentiment score positivo seguito dal 
+#	sentiment score negativo separati da uno spazio.
 def get_senti_score(corpus):
 	senti_score = sentiment_o.SentiStrengthgetScore(corpus)
 	
@@ -22,19 +29,21 @@ def stop_JVM():
 ###############
 
 
-# From a CSV file calculate the positive/negative sentiment score and output the results in a CSV file
-#	param:
-#		file_name = input CSV file
-#		output_name = output CSV file
-#		postId = string containing the field, on the file_name, of the id
-#		textfield = string of the field to analyze for sentiment
+# Calcola il sentiment score positivo ed il sentiment score negativo per ogni testo contenuto 
+# nel file di input.
 #
-#	output CSV file format:
-#		Id
-#		SentimentPositiveScore = {1, 2, 3, 4, 5} (default 'None' if something goes wrong)
-#		SentimentNegativeScore = {-1, -2, -3, -4, -5} (default 'None' if something goes wrong)
-
-def sentiment(file_name, output_file, postId='PostId', textfield='Corpus'):
+# parametri:
+#	file_name: nome del file csv contenente i testi da analizzare, deve contenere almeno:
+#			- 'PostId'
+#			- il campo con il testo da analizzare
+#	output_file: nome del file csv su cui scrivere i risultati, conterrà i campi:
+#			- 'PostId'
+#			- 'SentimentPositiveScore' con valore nell'intervallo [1,5], conterrà valore
+#				'None' se il calcolo non va a buon fine
+#			- 'SentimentNegativeScore' con valore nell'intervallo [-1,-5], conterrà valore
+#				'None' se il calcolo non va a buon fine
+#	text_field: nome del campo che contiene il testo che si vuole analizzare
+def sentiment(file_name, output_file, textfield='Corpus'):
 	dict_reader = csv.DictReader(open(file_name, 'r'), delimiter=';')
 	
 	f = [postId, 'SentimentPositiveScore', 'SentimentNegativeScore']
@@ -48,7 +57,7 @@ def sentiment(file_name, output_file, postId='PostId', textfield='Corpus'):
 		total += 1
 		corpus = row[textfield]
 		r = {}
-		r[postId] = row[postId]
+		r['PostId'] = row['PostId']
 		r['SentimentPositiveScore'] = str('None')
 		r['SentimentNegativeScore'] = str('None')
 		try:
@@ -69,7 +78,7 @@ def sentiment(file_name, output_file, postId='PostId', textfield='Corpus'):
 			count += 1
 		except Exception, e:
 			print e
-			print "Exception on id ", row[postId]
+			print "Exception on id ", row['PostId']
 			dict_writer.writerow(r)
 			skipped += 1
 			continue
@@ -82,31 +91,28 @@ def sentiment(file_name, output_file, postId='PostId', textfield='Corpus'):
 	return 'Done'
 
 
-
-
-
-# function sentiment specialized for the case of the comment
+# Calcola il sentiment score positivo ed il sentiment score negativo per ogni testo contenuto 
+# nel file di input.
 #
-#	input CSV file format (the output file of the function userscommentsonquestions_dataset from the module builddataset):
-#		PostId
-#		NumberOfUsersComment
-#		TextOfUsersComments
-#
-#	output CSV file format:
-#		PostId
-#		NumberOfUsersComment
-#		CommentSentimentPositiveScore = {1, 2, 3, 4, 5} (default 'None' if something goes wrong)
-#		CommentSentimentNegativeScore = {-1, -2, -3, -4, -5} (default 'None' if something goes wrong)
-
+# parametri:
+#	file_name: nome del file csv contenente i testi da analizzare, deve contenere almeno:
+#			- 'PostId'
+#			- 'TextOfUsersComments' contiene il testo da analizzare
+#			- 'NumberOfUsersComments'
+#	output_file: nome del file csv su cui scrivere i risultati, conterrà i campi:
+#			- 'PostId'
+#			- 'NumberOfUsersComments'
+#			- 'CommentSentimentPositiveScore' con valore nell'intervallo [1,5] risultato del calcolo
+#				sul campo 'TextOfUsersComments', conterrà valore 'None' se il calcolo non va a buon fine
+#			- 'CommentSentimentNegativeScore' con valore nell'intervallo [-1,-5] risultato del calcolo
+#				sul campo 'TextOfUsersComments', conterrà valore 'None' se il calcolo non va a buon fine
 def sentiment_comments(file_name, output_file):
 	dict_reader = csv.DictReader(open(file_name, 'r'), delimiter=';')
 	
 	head = dict_reader.fieldnames
-	#head.append('Corpus')
+	
 	f = ['PostId', 'NumberOfUsersComments', 'CommentSentimentPositiveScore', 'CommentSentimentNegativeScore']
-	#head.append('SentimentPositiveScore')
-	#head.append('SentimentNegativeScore')
-
+	
 	dict_writer = csv.DictWriter(open(output_file, 'w'), delimiter=';', fieldnames=f)
 	dict_writer.writerow(dict((fn,fn) for fn in f)) #Scrive gli header
 	total = 0
@@ -127,11 +133,10 @@ def sentiment_comments(file_name, output_file):
 			try:
 				corpus = corpus.decode('unicode_escape').encode('ascii','ignore')
 			except UnicodeDecodeError:
-				#print r['PostId']
 				try:
 					corpus = unicode(corpus).encode('ascii', 'ignore')
 				except Exception:
-					corpus = unicode(corpus, errors='ignore') #smart_str(corpus)
+					corpus = unicode(corpus, errors='ignore')
 
 			score = get_senti_score(corpus)
 			
@@ -154,16 +159,11 @@ def sentiment_comments(file_name, output_file):
 	print "Skipped: ", skipped
 	return 'Done'
 
-# Automaticly start a JVM, you can comment the next line and start the JVM when you need it
+# Avvia la JVM nel momento in cui viene importato il file come libreria, si può commentare tale riga e avviarla solo quando necessario
 start_JVM()
 
-# S = class Sentiment
+# S = carica la classe Sentiment
 S = jpype.JClass('Sentiment')
 
-# sentiment_o = object of the class Sentiment
+# sentiment_o = oggetto della classe Sentiment
 sentiment_o = S()
-
-#sentiment('academia_questions.csv', 'ac_sentiment.csv')
-#sentiment('stackoverflow_questions.csv', 'so_sentiment.csv')
-#sentiment('dataset_liwcsenti.csv', 'ac_sentiment.csv')
-
